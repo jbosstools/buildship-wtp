a52095c6f86aa877b0654b8859e69e49f3ed5c96
Ian Stewart-Binks <istewart@redhat.com>
2015-07-30 14:13:00 -0400

    Add handler/pluxin.xml/META-INF
    Signed-off-by: Ian Stewart-Binks <istewart@redhat.com>

---
+++ javaee.core/src/javaee/core/Handler.java
@@ -0,0 +1,67 @@
+package javaee.core;
+
+import java.util.List;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IAdapterManager;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableList.Builder;
+
+/**
+ * Our sample handler extends AbstractHandler, an IHandler base class.
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
+public class Handler extends AbstractHandler {
+	/**
+	 * The constructor.
+	 */
+	public Handler() {
+	}
+
+	/**
+	 * the command has been executed, so extract extract the needed information
+	 * from the application context.
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+
+		List<IProject> projects = collectSelectedProjects(event);
+		
+		MessageDialog.openInformation(
+				window.getShell(),
+				"Jboss-gradle",
+				"..");
+		
+		return null;
+	}
+	
+    private List<IProject> collectSelectedProjects(ExecutionEvent event) {
+        ISelection currentSelection = HandlerUtil.getCurrentSelection(event);
+        Builder<IProject> result = ImmutableList.builder();
+        if (currentSelection instanceof IStructuredSelection) {
+            IStructuredSelection selection = (IStructuredSelection) currentSelection;
+            IAdapterManager adapterManager = Platform.getAdapterManager();
+            for (Object selectionItem : selection.toList()) {
+                @SuppressWarnings({"cast", "RedundantCast"})
+                IResource resource = (IResource) adapterManager.getAdapter(selectionItem, IResource.class);
+                if (resource != null) {
+                    IProject project = resource.getProject();
+                    result.add(project);
+                }
+            }
+        }
+        return result.build();
+    }
+}
