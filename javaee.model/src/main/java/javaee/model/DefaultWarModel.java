758546d34f22512d4985cc7b7369d9296cb91993
Ian Stewart-Binks <istewart@redhat.com>
2015-07-30 14:54:50 -0400

    Add model and plugin
    Signed-off-by: Ian Stewart-Binks <istewart@redhat.com>

---
+++ javaee.model/src/main/java/javaee/model/DefaultWarModel.java
@@ -0,0 +1,36 @@
+package javaee.model;
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.List;
+import org.gradle.api.plugins.WarPlugin;
+
+public class DefaultWarModel implements Serializable {
+	private final List<String> pluginClassNames;
+	private final File webAppDir;
+	private final String webAppDirName;
+	private final String webXml;
+
+	public DefaultWarModel(List<String> pluginClassNames, File webAppDir, String webAppDirName, String webXml) {
+		this.pluginClassNames = pluginClassNames;
+		this.webAppDir = webAppDir;
+		this.webAppDirName = webAppDirName;
+		this.webXml = webXml;
+	}
+
+	public boolean hasWarPlugin() {
+		return pluginClassNames.contains(WarPlugin.class.getName());
+	}
+
+	public File getWebAppDir() {
+		return this.webAppDir;
+	}
+
+	public String getWebAppDirName() {
+		return this.webAppDirName;
+	}
+
+	public String getWebXmlName() {
+		return this.webXml;
+	}
+}
---
+++ javaee.model/src/main/java/javaee/model/WarModel.java
@@ -0,0 +1,5 @@
+package javaee.model;
+
+public class WarModel {
+
+}
