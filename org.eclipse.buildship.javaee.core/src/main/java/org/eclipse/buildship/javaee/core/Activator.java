f616dd68493de83d495c1fbc1caaeaf4d95801b1
Ian Stewart-Binks <istewart@redhat.com>
2015-09-24 17:55:01 -0400

    Make Buildship-Javaee update site usable
    Signed-off-by: Ian Stewart-Binks <istewart@redhat.com>

--- org.eclipse.buildship.javaee.core/src/main/java/org/eclipse/buildship/javaee/core/Activator.java
+++ org.eclipse.buildship.javaee.core/src/main/java/org/eclipse/buildship/javaee/core/Activator.java
@@ -17,14 +17,22 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.channels.FileChannel;
+import java.util.Dictionary;
 
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+import org.eclipse.buildship.core.Logger;
+import org.eclipse.buildship.core.util.logging.EclipseLogger;
+
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.util.tracker.ServiceTracker;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -33,28 +41,53 @@ public class Activator extends AbstractUIPlugin {
 
     public static final String PLUGIN_ID = "org.eclipse.buildship.javaee.core"; //$NON-NLS-1$
     private static Activator plugin;
+    private ServiceRegistration loggerService;
+    private ServiceTracker loggerServiceTracker;
 
     @Override
     public void start(BundleContext context) throws Exception {
         super.start(context);
 
         plugin = this;
+        this.loggerService = registerService(context, Logger.class, createLogger(), null);
+        this.loggerServiceTracker = createServiceTracker(context, Logger.class);
+
         copyGradlePluginFilesToMetadataFolder();
+    }
+
+    private <T> ServiceRegistration registerService(BundleContext context, Class<T> clazz, T service, Dictionary<String, Object> properties) {
+        return context.registerService(clazz.getName(), service, properties);
+    }
+
+    private ServiceTracker createServiceTracker(BundleContext context, Class<?> clazz) {
+        ServiceTracker serviceTracker = new ServiceTracker(context, clazz.getName(), null);
+        serviceTracker.open();
+        return serviceTracker;
     }
 
     @Override
     public void stop(BundleContext context) throws Exception {
         plugin = null;
         super.stop(context);
+        this.loggerService.unregister();
+    }
+
+    private EclipseLogger createLogger() {
+        return new EclipseLogger(getLog(), PLUGIN_ID, isDebugging());
     }
 
     public static Activator getDefault() {
+        getLogger().error("Test Error...");
         return plugin;
     }
 
+    public static Logger getLogger() {
+        return (Logger) getInstance().loggerServiceTracker.getService();
+    }
+
     /**
-     * Copies the init.gradle file and the Gradle plug-in from the Core plug-in directory
-     * to the workspace's .metadata folder.
+     * Copies the init.gradle file and the Gradle plug-in from the Core plug-in directory to the
+     * workspace's .metadata folder.
      */
     private void copyGradlePluginFilesToMetadataFolder() throws Exception {
         IPath metadataPath = this.getStateLocation();
@@ -63,20 +96,21 @@ public class Activator extends AbstractUIPlugin {
 
         Bundle bundle = Platform.getBundle(PLUGIN_ID);
         URL initUrl = bundle.getEntry("init.gradle");
-        URL pluginUrl = bundle.getEntry("repo/libs/redhat-plugin-1.0.jar");
+        URL pluginUrl = bundle.getEntry("/repo/libs/redhat-plugin-1.0.jar");
         File initFile = null;
         File pluginFile = null;
 
-        initFile = new File(FileLocator.resolve(initUrl).toURI());
-        pluginFile = new File(FileLocator.resolve(pluginUrl).toURI());
+        System.out.println("Init URL   : " + initUrl);
+        System.out.println("Plug-in URL: " + pluginUrl);
+        initFile = new File(FileLocator.toFileURL(initUrl).toURI());
+        pluginFile = new File(FileLocator.toFileURL(pluginUrl).toURI());
 
         copyFile(initFile, new File(initGradlePath.toString()));
         copyFile(pluginFile, new File(pluginPath.toString()));
     }
 
     /**
-     * Returns an image descriptor for the image file at the given
-     * plug-in relative path
+     * Returns an image descriptor for the image file at the given plug-in relative path
      */
     public static ImageDescriptor getImageDescriptor(String path) {
         return imageDescriptorFromPlugin(PLUGIN_ID, path);
@@ -84,8 +118,7 @@ public class Activator extends AbstractUIPlugin {
 
     @SuppressWarnings("resource")
     /**
-     * Copies the contents of sourceFile into destFile. Creates destFile if it does
-     * not yet exist.
+     * Copies the contents of sourceFile into destFile. Creates destFile if it does not yet exist.
      */
     public static void copyFile(File sourceFile, File destFile) throws IOException {
         if (!destFile.exists()) {
