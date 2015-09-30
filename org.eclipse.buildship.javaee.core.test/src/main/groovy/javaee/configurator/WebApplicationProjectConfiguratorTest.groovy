f40eac7488c01b34dc5abb2b7b4feb84962d9767
Ian Stewart-Binks <istewart@redhat.com>
2015-09-30 12:36:50 -0400

    Add java facet test
    Signed-off-by: Ian Stewart-Binks <istewart@redhat.com>

--- org.eclipse.buildship.javaee.core.test/src/main/groovy/javaee/configurator/WebApplicationProjectConfiguratorTest.groovy
+++ org.eclipse.buildship.javaee.core.test/src/main/groovy/javaee/configurator/WebApplicationProjectConfiguratorTest.groovy
@@ -15,72 +15,33 @@ import org.eclipse.jst.common.project.facet.core.internal.JavaFacetUtil;
 import org.eclipse.wst.common.project.facet.core.IFacetedProject
 import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
 import com.gradleware.tooling.toolingclient.GradleDistribution
+import org.eclipse.buildship.core.workspace.SynchronizeGradleProjectJob
 import org.eclipse.core.runtime.NullProgressMonitor
 import org.eclipse.buildship.core.util.progress.AsyncHandler
 
-class WebApplicationProjectConfiguratorTest extends Specification {
-
-
-    @Rule
-    TemporaryFolder tempFolder
+class WebApplicationProjectConfiguratorTest extends ProjectImportSpecification {
 
     def "Java facet is added to War Project"() {
         def monitor = new NullProgressMonitor()
-
-        given:
         IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
-        File projectLocation = newProject(false, true)
-        def job = newProjectImportJob(projectLocation)
+        setup:
+        def location = folder('app')
+        folder('app', 'src', 'main', 'java')
+        file('app', 'settings.gradle') << ''
+        file('app', 'build.gradle') <<
+        '''apply plugin: "war"'''
+        file('app', 'src/main/java/TestClass.java') << ''
 
         when:
-        job.schedule()
-        job.join()
+        executeProjectImportAndWait(location)
+        Thread.sleep(60000)
 
         then:
-        def project = workspaceRoot.getProject(projectLocation.name);
-        1 == 1
+        def project = workspaceRoot.getProject('app');
         IFacetedProject facetedProject = ProjectFacetsManager.create(project, true, monitor);
         IProjectFacetVersion javaFacetVersion = JavaFacet.FACET.getVersion(JavaFacetUtil.getCompilerLevel(project));
+
         facetedProject.hasProjectFacet(javaFacetVersion)
-    }
-
-
-    def newProject(boolean projectDescriptorExists, boolean applyWarPlugin) {
-        def root = tempFolder.newFolder('simple-project')
-        new File(root, 'build.gradle') << (applyWarPlugin ? 'apply plugin: "war"' : '')
-        new File(root, 'settings.gradle') << ''
-        new File(root, 'src/main/webapp').mkdirs()
-        new File(root, '.settings').mkdirs()
-//        new File(root.toString() + '/.settings', 'gradle.prefs') << ''
-
-        if (projectDescriptorExists) {
-            new File(root, '.project') << '''<?xml version="1.0" encoding="UTF-8"?>
-                <projectDescription>
-                  <name>simple-project</name>
-                  <comment>original</comment>
-                  <projects></projects>
-                  <buildSpec></buildSpec>
-                  <natures></natures>
-                </projectDescription>'''
-            if (applyWarPlugin) {
-                new File(root, '.classpath') << '''<?xml version="1.0" encoding="UTF-8"?>
-                    <classpath>
-                      <classpathentry kind="con" path="org.eclipse.jdt.launching.JRE_CONTAINER"/>
-                      <classpathentry kind="src" path="src/main/java"/>
-                      <classpathentry kind="output" path="bin"/>
-                    </classpath>'''
-            }
-        }
-        root
-    }
-
-    def newProjectImportJob(File location) {
-        ProjectImportConfiguration configuration = new ProjectImportConfiguration()
-        configuration.gradleDistribution = GradleDistributionWrapper.from(GradleDistribution.fromBuild())
-        configuration.projectDir = location
-        configuration.applyWorkingSets = true
-        configuration.workingSets = []
-//        new ProjectImportJob(configuration, AsyncHandler.NO_OP)
     }
 
 }
